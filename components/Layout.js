b7835b31bf596630170e7397babf8f7c43d97cc7
Matevosyan Mko <mm.gucho@gmail.com>
2023-02-04 17:03:29 +0400

    fix

--- components/Layout.js
+++ components/Layout.js
@@ -10,20 +10,23 @@ const Layout = ({ children, i18n }) => {
     <Fragment>
       <LanguageSwitcher i18n={i18n} />
       <Menu />
-      <div className='container'>
-        <div className='relative bg-white shadow-lg scale-in-center'>
-          <div className='absolute w-full left-0 md:-top-75px xs:-top-50px '>
+      <div className="container">
+        <div className="relative bg-white shadow-lg scale-in-center rounded-lg">
+          <div className="absolute w-full left-0 md:-top-75px xs:-top-50px ">
             <Link href={`/${language}`}>
               <a>
                 <img
-                  src='/images/avatar.jpg'
-                  className='md:w-150px md:h-150px xs:w-100px xs:h-100px rounded-full m-auto  transition-all duration-300 hover:animate-border border-2 border-transparent '
-                  alt='mkrtich matevosyan'
+                  src="/images/avatar.jpg"
+                  className="md:w-150px md:h-150px xs:w-100px xs:h-100px rounded-full m-auto  transition-all duration-300 hover:animate-border border-2 border-transparent "
+                  alt="mkrtich matevosyan"
                 />
               </a>
             </Link>
           </div>
-          <div className='text-primary-dark  py-75px px-4' children={children} />
+          <div
+            className="text-primary-dark  py-75px px-4"
+            children={children}
+          />
         </div>
       </div>
     </Fragment>
